1298dda721290022dbe10c0e352c97e757501cd0
Bryan Iago <160327635+Bryanzinn81@users.noreply.github.com>
2025-03-14 23:56:31 +0000

    finalizando com tudo funcionando

--- script.js
+++ script.js
@@ -24,14 +24,15 @@ function Vitoria(){
     const parametrosV = [
         [0,1,2], [3,4,5],[6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8],[2,4,6]
     ];
-    for (let parametros of parametrosV ){
-        let [a,b,c] = parametros;
+    for (let i = 0; i < parametrosV.length; i++){
+        let [a,b,c] = parametrosV[i];
+        if(tabuleiro[a]&& tabuleiro[a] === tabuleiro[b] && tabuleiro[a] === tabuleiro[c]){
+            alert(`Jogador ${tabuleiro[a]} venceu!!!!!`);
+            resetarJogo();
+            return;
+        }
     }
-    if(tabuleiro[a]&& tabuleiro[a] === tabuleiro[b] && tabuleiro[a] === tabuleiro[c]){
-        alert("Jogador ${tabuleiro[a]} venceu!!!!!")
-        resetarJogo();
-        return;
-    }
+    
     if(!tabuleiro.includes("")){
         alert("empate!!!!");
         resetarJogo();
