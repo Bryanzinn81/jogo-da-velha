075ffc7188cf1cde419ecc9ff54610bfbf8b1a79
Bryan Iago <160327635+Bryanzinn81@users.noreply.github.com>
2025-03-14 20:46:44 +0000

    finalizando o jogo da velha

---
+++ script.js
@@ -0,0 +1,49 @@
+const quadradosC = document.querySelectorAll(".quadrado");
+const resetar = document.getElementById("reset");
+let jogadorAtual = "X";
+let tabuleiro = ["","","","","","","","",""];
+
+quadradosC.forEach(quadrado => quadrado.addEventListener("click", click));
+
+
+function click(event){
+    const quadrado = event.target;
+    const index = quadrado.dataset.index;
+
+    if(tabuleiro[index] ===""){
+        tabuleiro[index] = jogadorAtual;   
+        quadrado.textContent = jogadorAtual;
+    }
+    console.log;
+    Vitoria();
+    jogadorAtual = jogadorAtual === "X" ? "O" : "X";
+    }
+    console.log;
+
+function Vitoria(){
+    const parametrosV = [
+        [0,1,2], [3,4,5],[6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8],[2,4,6]
+    ];
+    for (let parametros of parametrosV ){
+        let [a,b,c] = parametros;
+    }
+    if(tabuleiro[a]&& tabuleiro[a] === tabuleiro[b] && tabuleiro[a] === tabuleiro[c]){
+        alert("Jogador ${tabuleiro[a]} venceu!!!!!")
+        resetarJogo();
+        return;
+    }
+    if(!tabuleiro.includes("")){
+        alert("empate!!!!");
+        resetarJogo();
+    }
+}
+
+
+
+function resetarJogo(){
+    tabuleiro = ["", "", "", "", "", "", "", "", ""]
+
+    quadradosC.forEach(quadrado =>quadrado.textContent="");
+    jogadorAtual = "X";
+}
+resetar.addEventListener("click",resetarJogo);
